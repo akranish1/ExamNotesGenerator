bc1cbb6d26d51df1503e6b5121bb507bde507b05
akranish1 <akranish0@gmail.com>
2026-04-04 23:25:27 +0530

    deployment1a

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,8 +12,9 @@ export const googleAuth = async(req,res)=>{
         let token=await getToken(user._id);
         res.cookie("token",token,{
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-        samesite: "strict",
+            secure: true,
+        sameSite: "strict",
+        path:'/',
         maxAge: 7*24*60*60*1000 //7 days
         });
         return res.status(200).json(user);
